a7ccf98298e77342c6a7bbc1c5797cb6243f5e99
miurann <mn.camissha@gmail.com>
2023-01-23 03:27:25 +0900

    fix:クイズページのUIを整頓。メニューバーも整え、アイコン表示でcarrierwave導入 #9

--- Gemfile
+++ Gemfile
@@ -42,6 +42,7 @@ gem "bootsnap", require: false
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
+gem 'carrierwave'
 gem 'cssbundling-rails'
 gem 'enum_help'
 gem 'foreman'
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,5 @@
 class QuestionsController < ApplicationController
+  layout 'questions_menu', only: %i[show]
   def index
   end
 
@@ -6,5 +7,7 @@ class QuestionsController < ApplicationController
     @question = Question.find(params[:id])
     @choices = @question.choices
     @correct_choice = Choice.includes(:question).find_by(correct_answer: true, question: {id: params[:id]})
+    questions_region = @question.wine.region_id
+    @questions = Question.includes(:wine).where(wine: {region_id: questions_region})
   end
 end
