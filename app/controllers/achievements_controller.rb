6f57277c5a439f3bd8cfca6383874bca3cdeb10e
miurann <mn.camissha@gmail.com>
2023-02-05 23:48:40 +0900

    add: achievements_controllerの編集、解説ページへの遷移 "9

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,12 +1,13 @@
 class AchievementsController < ApplicationController
   def create
-    @question = Question.find()
-    choice = Choice.find(params[:choice_id])
+    question = Question.find(params[:question_id])
+    @choices = question.choices
+    choice = @choices.find_by(correct_answer: true)
     if current_user.answered?(choice)
-      redirect_back fallback_location: @question
+      redirect_to quiz_question_description_path(question_id: params[:question_id], quiz_id: params[:quiz_id])
     else
-      current_user.correct(choice)
-      redirect_back fallback_location: @question
+      current_user.answer(choice)
+      redirect_to quiz_question_description_path(question_id: params[:question_id], quiz_id: params[:quiz_id])
     end
   end
 end
