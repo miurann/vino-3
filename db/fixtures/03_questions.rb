076cf8d514e7e4e73abd3767c1f256e1fb6eaa57
miurann <mn.camissha@gmail.com>
2023-05-15 18:34:54 +0900

    fix:ワイン詳細ページのレイアウト崩れを修正

---
+++ db/fixtures/03_questions.rb
@@ -0,0 +1,47 @@
+Question.seed do |s|
+  s.id          = 1
+  s.content     = 'テヌータ・サン・アントニオ アマローネ・デッラ・ヴァルポリチェッラ セレツィオーネ アントニオ・カスタグネディはどんな種類のワインでしょうか？'
+  s.description = 'アマローネは陰干しぶどうを使った赤ワインです'
+  s.wine_id     = 1
+  s.region_id   = 6
+end
+
+Question.seed do |s|
+  s.id          = 2
+  s.content     = 'ワインの第2問出題テストです'
+  s.description = '第2問の正解解説です'
+  s.wine_id     = 2
+  s.region_id   = 6
+end
+
+Question.seed do |s|
+  s.id          = 3
+  s.content     = 'ワインの第3問出題テストです'
+  s.description = '第3問の正解解説です'
+  s.wine_id     = 3
+  s.region_id   = 3
+end
+
+Question.seed do |s|
+  s.id          = 4
+  s.content     = 'ワインの第4問出題テストです'
+  s.description = '第4問の正解解説です'
+  s.wine_id     = 4
+  s.region_id   = 2
+end
+
+Question.seed do |s|
+  s.id          = 5
+  s.content     = 'ワインの第5問出題テストです'
+  s.description = '第5問の正解解説です'
+  s.wine_id     = 5
+  s.region_id   = 2
+end
+
+Question.seed do |s|
+  s.id          = 6
+  s.content     = 'ワインの第6問出題テストです'
+  s.description = '第6問の正解解説です'
+  s.wine_id     = 6
+  s.region_id   = 2
+end
