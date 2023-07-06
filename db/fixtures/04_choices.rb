d296d3b636d49a006332109f96986a87ad68eda2
miurann <mn.camissha@gmail.com>
2023-07-06 14:15:42 +0900

    add:クイズデータ追加

---
+++ db/fixtures/04_choices.rb
@@ -0,0 +1,27 @@
+Choice.seed do |s|
+  s.id             = 1
+  s.content        = '「こい！こい！！こい！！！」'
+  s.correct_answer = false
+  s.question_id    = 1
+end
+
+Choice.seed do |s|
+  s.id             = 2
+  s.content        = '「ある！ある！！ある！！！」'
+  s.correct_answer = true
+  s.question_id    = 1
+end
+
+Choice.seed do |s|
+  s.id             = 3
+  s.content        = '「そうだ！そうだ！！そうだ！！！」'
+  s.correct_answer = false
+  s.question_id    = 1
+end
+
+Choice.seed do |s|
+  s.id             = 4
+  s.content        = '「ワイン！ワイン！！ワイン！！！」'
+  s.correct_answer = false
+  s.question_id    = 1
+end
